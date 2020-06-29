67764a53844d1349f64dcf8c8d04b214bce63345
deepak-delta <ddk3857@gmail.com>
2020-06-29 17:44:19 +0530

    Section 3.11

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -8,9 +8,40 @@
                 <div class="card-header">All Categories</div>
 
                 <div class="card-body">
-                  @foreach($cate as $ca)
-                <p>{{$ca->name}}</p>
-                  @endforeach
+                  
+                  <table class="table table-striped">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @if(count($cate)>0)  
+                      @foreach($cate as $key=>$ca)
+                        <tr>
+                        <th scope="row">{{$key+1}}</th>
+                        <td>{{$ca->name}}</td>
+                            <td>
+                                <a href="{{route('category.edit',[$ca->id])}}">
+                                    <button type="submit" class="btn btn-outline-success">Edit</button>
+                                </a>
+                            </td>
+                            <td>
+                                <a href="">
+                                    <button type="submit" class="btn btn-outline-danger">Delete</button>
+                                </a>
+                            </td>
+                        </tr>
+                      @endforeach 
+                      @else
+                        <td>No Data</td>
+                      @endif  
+                      
+                    </tbody>
+                  </table>
                 </div>
             </div>
         </div>
