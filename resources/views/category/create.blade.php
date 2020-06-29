950f5291fca1118544adff360e52077f31e2359f
deepak-delta <ddk3857@gmail.com>
2020-06-29 17:44:55 +0530

    Section 3.11

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -5,7 +5,11 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
          @if(Session::has('message'))
-            <div class="alert alert-success">{{Session::get('message')}}</div>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">{{Session::get('message')}}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
          @endif   
         <form action="{{route('category.store')}}" method="post">@csrf
             <div class="card">
